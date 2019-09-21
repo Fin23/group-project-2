84bfecba4ae442d568e86c3ec1ec70d7232b5f41
Ross Jameson <Rossjameson1623@gmail.com>
2019-09-21 13:46:43 -0500

    finishing html and css for home page, starting to link other html pages

---
+++ app/public/js/index.js
@@ -0,0 +1,25 @@
+
+$(window).scroll(function(){
+    $("header").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
+$(window).scroll(function(){
+    $(".topnav").css("opacity", 1 - $(window).scrollTop() / 350);
+  });
+$(window).scroll(function(){
+    $(".row1").css("opacity", 1 - $(window).scrollTop() / 450);
+  });
+$(window).scroll(function(){
+    $(".row2").css("opacity", 1 - $(window).scrollTop() / 1000);
+  });
+$(window).scroll(function(){
+    $(".row3").css("opacity", 1 - $(window).scrollTop() / 1900);
+  });
+$(window).scroll(function(){
+    $(".row4").css("opacity", 1 - $(window).scrollTop() / 2200);
+  });
+$(window).scroll(function(){
+    $(".row5").css("opacity", 1 - $(window).scrollTop() / 3200);
+  });
+$(window).scroll(function(){
+    $(".row6").css("opacity", 1 - $(window).scrollTop() / 3200);
+  });

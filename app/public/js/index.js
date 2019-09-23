26497845ab13c11ba6c2cf527f9eaf99182cb29a
Ross Jameson <Rossjameson1623@gmail.com>
2019-09-23 09:38:09 -0500

    finishing html and css home page

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,6 +1,6 @@
 
 $(window).scroll(function(){
-    $("header").css("opacity", 1 - $(window).scrollTop() / 250);
+    $(".title").css("opacity", 1 - $(window).scrollTop() / 250);
   });
 $(window).scroll(function(){
     $(".topnav").css("opacity", 1 - $(window).scrollTop() / 350);
@@ -8,18 +8,23 @@ $(window).scroll(function(){
 $(window).scroll(function(){
     $(".row1").css("opacity", 1 - $(window).scrollTop() / 450);
   });
-$(window).scroll(function(){
-    $(".row2").css("opacity", 1 - $(window).scrollTop() / 1000);
-  });
-$(window).scroll(function(){
-    $(".row3").css("opacity", 1 - $(window).scrollTop() / 1900);
-  });
-$(window).scroll(function(){
-    $(".row4").css("opacity", 1 - $(window).scrollTop() / 2200);
-  });
-$(window).scroll(function(){
-    $(".row5").css("opacity", 1 - $(window).scrollTop() / 3200);
-  });
-$(window).scroll(function(){
-    $(".row6").css("opacity", 1 - $(window).scrollTop() / 3200);
-  });
+
+// $(window).scroll(function(){
+//     $(".row2").css("opacity", 1 - $(window).scrollTop() / 1000);
+//   });
+
+// $(window).scroll(function(){
+//     $(".row3").css("opacity", 1 - $(window).scrollTop() / 1900);
+//   });
+// $(window).scroll(function(){
+//     $(".row4").css("opacity", 1.1 - $(window).scrollTop() / 2200);
+//   });
+// $(window).scroll(function(){
+//     $(".row5").css("opacity", 2 - $(window).scrollTop() / 3200);
+//   });
+// $(window).scroll(function(){
+//     $(".row6").css("opacity", 1.1 - $(window).scrollTop() / 3200);
+//   });
+
+
+  
